f57d1bcb42203f39f318b57ea8a1b2eb30835c6e
mlkaman <cyheddie@gmail.com>
2020-01-04 07:47:40 -0500

    Added scripts + prefabs for managing VR interface layer

---
+++ Assets/Scripts/CreateWalls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR; // Definitions for OpenVR references
+
+public class CreateWalls : MonoBehaviour
+{
+
+    public GameObject llCorner;
+    public GameObject lrCorner;
+    public GameObject ulCorner;
+    public GameObject urCorner;
+    public GameObject wall;
+
+    public void Create()
+    {
+        print("CreateWalls::Create()");
+        HmdQuad_t boundaryRect = VRInterface.chaperoneRect;
+        print("vCorners0: " + boundaryRect.vCorners0.v0 + ", " + boundaryRect.vCorners0.v1 + ", " + boundaryRect.vCorners0.v2);
+        print("vCorners1: " + boundaryRect.vCorners1.v0 + ", " + boundaryRect.vCorners1.v1 + ", " + boundaryRect.vCorners1.v2);
+        print("vCorners2: " + boundaryRect.vCorners2.v0 + ", " + boundaryRect.vCorners2.v1 + ", " + boundaryRect.vCorners2.v2);
+        print("vCorners3: " + boundaryRect.vCorners3.v0 + ", " + boundaryRect.vCorners3.v1 + ", " + boundaryRect.vCorners3.v2);
+        Instantiate(llCorner, new Vector3(boundaryRect.vCorners0.v0, boundaryRect.vCorners0.v1, boundaryRect.vCorners0.v2), Quaternion.identity);
+        Instantiate(lrCorner, new Vector3(boundaryRect.vCorners1.v0, boundaryRect.vCorners1.v1, boundaryRect.vCorners1.v2), Quaternion.identity);
+        Instantiate(ulCorner, new Vector3(boundaryRect.vCorners2.v0, boundaryRect.vCorners2.v1, boundaryRect.vCorners2.v2), Quaternion.identity);
+        Instantiate(urCorner, new Vector3(boundaryRect.vCorners3.v0, boundaryRect.vCorners3.v1, boundaryRect.vCorners3.v2), Quaternion.identity);
+    }
+
+}
---
+++ Assets/Scripts/VRInterface.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR; // Definitions for OpenVR references
+
+public class VRInterface : MonoBehaviour
+{
+    /* OpenVR Rectangle whose points represent the boundaries of the VR play area. */
+    public static HmdQuad_t chaperoneRect;
+
+    //public float travelingSpeed {get; private set;} // public getter but private setter
+
+    public bool SetChaperoneRect()
+    {
+        var chaperone = OpenVR.Chaperone;
+        bool success = (chaperone != null) && chaperone.GetPlayAreaRect(ref chaperoneRect);
+        return success;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!SetChaperoneRect())
+        {
+            print("Failed to get VR Play Area.  Should probably quit ?");
+        }
+        else
+        {
+            print("SetChaperoneRect success!");
+            //print("vCorners0: " + chaperoneRect.vCorners0.v0 + ", " + chaperoneRect.vCorners0.v1 + ", " + chaperoneRect.vCorners0.v2);
+            //print("vCorners1: " + chaperoneRect.vCorners1.v0 + ", " + chaperoneRect.vCorners1.v1 + ", " + chaperoneRect.vCorners1.v2);
+            //print("vCorners2: " + chaperoneRect.vCorners2.v0 + ", " + chaperoneRect.vCorners2.v1 + ", " + chaperoneRect.vCorners2.v2);
+            //print("vCorners3: " + chaperoneRect.vCorners3.v0 + ", " + chaperoneRect.vCorners3.v1 + ", " + chaperoneRect.vCorners3.v2);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
